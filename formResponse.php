63de50def921c2d9c36f072a95e16ff7b8fe235c
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-02-08 15:51:46 -0800

    Added variables that hold the value of the input and display it. Styled the display message too

--- formResponse.php
+++ formResponse.php
@@ -7,7 +7,54 @@
 </head>
 <body>
   <?php
-  echo "<h1>Thanks for the submission.</h1>";
-  echo "<h2>Save your responses for future records.</h2>"; ?>
+  $fname = $_GET["fname"];
+  $lname = $_GET["lname"];
+  $password = $_GET["password"];
+  $purpose = $_GET["purpose"];
+  $email = $_GET["email"];
+
+  echo '<style type="text/css">';
+  echo '
+  h1{ 
+    color: red;
+    font-size: 40px;
+    // width: 50%;
+    margin: 0 auto;
+    border: 1px solid blue;
+  }
+  .submission{
+    border: 2px double green;
+    border-radius: 12px;
+    width: 40%;
+    margin: 30px auto;
+    padding: 20px 30px;
+  }
+
+  .response p{
+    font-size: 18px;
+  }
+
+  .response p:hover{
+    background-color: rgba(210, 210, 210, 0.9);
+    padding: 10px;
+    border-radius: 5px;
+  }
+  ';
+        
+      
+  echo '</style>';
+  echo "
+  <div class=\"submission\">
+  <h1>Thanks for the submission.</h1>
+  <div class=\"response\">
+  <p>Name: $fname $lname</p>
+  <p>Email: $email</p>
+  <p>Submitted SOP: $purpose</p>
+  <p>Password: $password</p>
+  </div>
+  <h2>Save your responses for future records.</h2>
+  </div>";
+  ?>
+
 </body>
 </html>
