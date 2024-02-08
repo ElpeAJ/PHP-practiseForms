053e8baac54f968f9043539073075165f4a1d5e1
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-02-08 15:08:40 -0800

    Added the DOCTYPE declaration to the php file

--- formResponse.php
+++ formResponse.php
@@ -1,2 +1,13 @@
-<h1>Thanks for the submission.</h1>
-<h2>Save your responses for future records.</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>response Form</title>
+</head>
+<body>
+  <?php
+  echo "<h1>Thanks for the submission.</h1>";
+  echo "<h2>Save your responses for future records.</h2>"; ?>
+</body>
+</html>
